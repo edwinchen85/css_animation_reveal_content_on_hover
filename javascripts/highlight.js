ccc987e379762a358a1ad92b9e8087b24929ebd6
edwinchen85 <edwinchen85@gmail.com>
2017-12-11 01:37:26 +0800

    Add and remove highlighted class on hover

--- javascripts/highlight.js
+++ javascripts/highlight.js
@@ -1,5 +1,13 @@
 $(function() {
 
-  
-  
+  $('.option').hover(
+    function() {
+      $('.option').removeClass('highlighted');
+      $(this).addClass('highlighted');
+    },
+    function() {
+      $(this).removeClass('highlighted');
+    }
+  )
+
 });
